4903fd31b62bc1807af75f9f071c2f2f52bab679
liuxuewen <947752894@qq.com>
2024-04-26 16:00:30 +0800

    add: 爱发电赞助用户详情

--- src/main/java/run/halo/afdian/service/SponsorService.java
+++ src/main/java/run/halo/afdian/service/SponsorService.java
@@ -1,34 +1,40 @@
 package run.halo.afdian.service;
 
-import com.nimbusds.jose.shaded.gson.JsonObject;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.json.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import run.halo.afdian.config.BaseSettingConfig;
 import run.halo.afdian.entity.SponsorEntity;
 import run.halo.afdian.utils.EncryptUtils;
-import run.halo.afdian.utils.RequestUtils;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
+@Slf4j
 public class SponsorService {
 
-    @Autowired
-    private RequestUtils requestUtils;
+    private final DefaultSettingFetcher settingFetcher;
 
-    @Autowired
-    private BaseSettingConfig baseSettingConfig;
+    public SponsorService(DefaultSettingFetcher settingFetcher) {
+        this.settingFetcher = settingFetcher;
+    }
 
     /**
      * 获取第一页的爱发电赞助用户
+     *
      * @return 赞助用户详情 {@link SponsorEntity}
      */
-    public SponsorEntity getSponsorList(){
+    public SponsorEntity getSponsorList() {
+
+        BaseSettingConfig baseSettingConfig =
+            settingFetcher.fetch(
+                    BaseSettingConfig.CONFIG_MAP_NAME,
+                    BaseSettingConfig.GROUP,
+                    BaseSettingConfig.class)
+                .orElseGet(BaseSettingConfig::new);
         String url = "https://afdian.net/api/open/query-sponsor";
-        Map<String, Object> params = new HashMap<>();
+        JSONObject params = new JSONObject();
         var timeInMillis = Calendar.getInstance().getTimeInMillis() / 1000;
 
         var userId = baseSettingConfig.getUserId();
@@ -38,11 +44,17 @@ public class SponsorService {
 
         String signMd5 = EncryptUtils.encrypt32(sign);
 
-        params.put("user_id", userId);
-        params.put("params", "{\"page\":1}");
-        params.put("ts", timeInMillis);
-        params.put("sign", signMd5);
-        return requestUtils.post(params, url, SponsorEntity.class);
+        params.set("user_id", userId);
+        params.set("params", "{\"page\":1}");
+        params.set("ts", timeInMillis);
+        params.set("sign", signMd5);
+
+        var execute = HttpRequest.post(url).body(params.toString()).execute();
+        var body = execute.body();
+        SponsorEntity sponsorEntity = new SponsorEntity();
+        BeanUtils.copyProperties(body, sponsorEntity, SponsorEntity.class);
+        log.info(sponsorEntity.toString());
+        return sponsorEntity;
     }
 
 }
