6e299a6e699a4d716e280547894f4ea02af08de4
liuxuewen <947752894@qq.com>
2024-04-29 11:18:58 +0800

    change: 优化依赖 delete: 删除hutool依赖

--- src/main/java/run/halo/afdian/service/SponsorService.java
+++ src/main/java/run/halo/afdian/service/SponsorService.java
@@ -1,16 +1,17 @@
 package run.halo.afdian.service;
 
-import cn.hutool.http.HttpRequest;
-import cn.hutool.json.JSONObject;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import run.halo.afdian.utils.EncryptUtils;
 import run.halo.app.plugin.ReactiveSettingFetcher;
-import java.util.Calendar;
 
 @Service
 @Slf4j
@@ -22,39 +23,37 @@ public class SponsorService {
         this.settingFetcher = settingFetcher;
     }
 
+    // 创建webClient
+    private WebClient webClient = WebClient.builder().baseUrl("https://afdian.net").build();
+
     /**
      * 获取第一页的爱发电赞助用户
-     *
      */
     public Mono<JsonNode> getSponsorList() {
-        return this.settingFetcher.get("basic").flatMap(base ->{
+        return this.settingFetcher.get("basic").flatMap(base -> {
             String token = base.get("token").asText();
             String userId = base.get("userId").asText();
-            String url = "https://afdian.net/api/open/query-sponsor";
-            JSONObject params = new JSONObject();
+            String url = "/api/open/query-sponsor";
+            Map<String, Object> params = new HashMap<>();
             var timeInMillis = Calendar.getInstance().getTimeInMillis() / 1000;
 
-            var sign = token.concat("params{\"page\":1}ts")
-                .concat(String.valueOf(timeInMillis)).concat("user_id").concat(userId);
+            var sign = token.concat("params{\"page\":1}ts").concat(String.valueOf(timeInMillis))
+                .concat("user_id").concat(userId);
 
             String signMd5 = EncryptUtils.encrypt32(sign);
 
-            params.set("user_id", userId);
-            params.set("params", "{\"page\":1}");
-            params.set("ts", timeInMillis);
-            params.set("sign", signMd5);
+            params.put("user_id", userId);
+            params.put("params", "{\"page\":1}");
+            params.put("ts", timeInMillis);
+            params.put("sign", signMd5);
 
-            var execute = HttpRequest.post(url).body(params.toString()).execute();
-            var body = execute.body();
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = null;
-            try {
-                jsonNode = objectMapper.readTree(body);
-            } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
-            }
-            log.info(jsonNode.toString());
-            return Mono.just(jsonNode);
+            var body =
+                webClient.post().uri(url).contentType(MediaType.APPLICATION_JSON)  // JSON数据类型
+                    .body(BodyInserters.fromValue(params))  // JSON字符串数据
+                    .retrieve() // 获取响应体
+                    .bodyToMono(JsonNode.class);// 响应数据类型转换
+            log.info(body.toString());
+            return body;
         });
 
     }
