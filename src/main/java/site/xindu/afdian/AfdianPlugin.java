f6fb8a83048f3b40931b288609e5201fd30cadd5
guqing <i@guqing.email>
2024-07-02 17:18:33 +0800

    chore: remove deprecated plugin wrapper

---
+++ src/main/java/site/xindu/afdian/AfdianPlugin.java
@@ -0,0 +1,13 @@
+package site.xindu.afdian;
+
+import org.springframework.stereotype.Component;
+import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
+
+@Component
+public class AfdianPlugin extends BasePlugin {
+
+    public AfdianPlugin(PluginContext pluginContext) {
+        super(pluginContext);
+    }
+}
